a66ed5c28deb5e5646bcdd9303dedecb0f6e62e7
software-engineer-bse <khurram.email.123@gmail.com>
2025-06-19 15:21:25 -0700

    Add Calculator

---
+++ Calculator/main.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let number = 0;
+
+function add(number1, number2) {
+    number = number1 + number2;
+    return number;
+}
+
+function sub(number1, number2) {
+    number = number1 - number2;
+    return number;
+}
+
+function mul(number1, number2) {
+    number = number1 * number2;
+    return number;
+}
+
+function div(number1, number2) {
+    number = number1 / number2;
+    return number;
+}
+
+// http://localhost:5000/api/add
+app.post("/api/add", (req, res) => {
+    let data = req.body;
+    number = add(data.number1, data.number2);
+    const r = {
+        message: "Ok",
+        result: number
+    }
+    res.status(201).json(r);
+
+});
+
+app.post("/api/sub", (req, res) => {
+    let data = req.body;
+    number = sub(data.number1, data.number2);
+
+    const r = {
+        message: "Ok",
+        result: number
+    }
+
+    res.status(201).json(r);
+
+});
+
+
+app.post("/api/mul", (req, res) => {
+    let data = req.body;
+    number = mul(data.number1, data.number2);
+    const r = {
+        message: "Ok",
+        result: number
+    }
+    res.status(201).json(r);
+});
+
+
+
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
